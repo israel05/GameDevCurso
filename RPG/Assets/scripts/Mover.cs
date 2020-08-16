e43972b0018631f737eaefc7e47284f1ea5a4c85
GRANDE-I7\idela <i.delamo@gmail.com>
2020-08-17 01:55:55 +0200

    RayCast creado, pero no se mueve

--- RPG/Assets/scripts/Mover.cs
+++ RPG/Assets/scripts/Mover.cs
@@ -7,6 +7,9 @@ public class Mover : MonoBehaviour
 {
 
     [SerializeField] Transform target;
+
+    Ray lastRay;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,14 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 10000);
+
+
         GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
