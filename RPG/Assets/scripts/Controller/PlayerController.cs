15a5f1a02d4a05fb489ab83bc0accf11d2920075
GRANDE-I7\idela <i.delamo@gmail.com>
2020-08-17 21:34:53 +0200

    Los enemigos son notificados si reciben un click

--- RPG/Assets/scripts/Controller/PlayerController.cs
+++ RPG/Assets/scripts/Controller/PlayerController.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+﻿using RPG.Combat;
+using RPG.Movement;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,17 +10,19 @@ namespace RPG.Control
 
     public class PlayerController : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
+
+
+        void Update()
         {
-        
+            InteractWithMovement();
+            InteractWithCombat();
         }
+
 
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit); //el parametro out, se pasa y se devuelve modificado 
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit); //el parametro out, se pasa y se devuelve modificado 
 
             if (hasHit)
             {
@@ -25,14 +30,35 @@ namespace RPG.Control
             }
         }
 
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
 
-        // Update is called once per frame
-        void Update()
+
+        private void InteractWithMovement()
         {
             if (Input.GetMouseButton(0))
             {
                 MoveToCursor();
             }
+        }
+
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue; //salta esta iteracion del loop
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+
+            }
+
         }
     }
 
