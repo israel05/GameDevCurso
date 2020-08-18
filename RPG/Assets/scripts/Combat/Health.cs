403f03fcd6a59319917d5c4a229e1e21b161bafd
GRANDE-I7\idela <i.delamo@gmail.com>
2020-08-19 00:06:28 +0200

    Corregido los muertos en el suelo

--- RPG/Assets/scripts/Combat/Health.cs
+++ RPG/Assets/scripts/Combat/Health.cs
@@ -6,23 +6,22 @@ namespace RPG.Combat{
 
     public class Health : MonoBehaviour
     {
-        [SerializeField] float health = 100f;
+        [SerializeField] float healthPoints = 100f;
         bool isDead = false;
         public void TakeDamage(float damage)
         {
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
+            if (healthPoints==0)
+            {
+                Die();
+            }      
+        }
 
-            if (!isDead)
-            {
-                health = Mathf.Max(health - damage, 0);
-                print("Enemigo con vida: " + health);
-            }
-           
-            if (health <= 0 && !isDead) 
-            {
-                isDead = true;           
-                GetComponent<Animator>().SetTrigger("die");
-            }
-            
+        private void Die()
+        {
+            if (isDead) return;
+            isDead = true;
+            GetComponent<Animator>().SetTrigger("die");
         }
     }
 }
