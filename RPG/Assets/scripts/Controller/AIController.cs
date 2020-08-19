fc883301b5b88e7250b532d0d1160157c489e2d5
GRANDE-I7\idela <i.delamo@gmail.com>
2020-08-19 14:08:10 +0200

    Gizmo y notificación de persecución

---
+++ RPG/Assets/scripts/Controller/AIController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+
+        private void Update()
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+          
+
+            float distanciaActual =    Vector3.Distance(transform.position, player.transform.position);
+            if( distanciaActual < chaseDistance)
+            {
+                print("Te veeeeeoooo" + name);
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
+        }
+
+    }
+
+}
