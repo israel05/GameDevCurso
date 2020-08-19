f146d3b5b3653aff648bc77ee1293fff8d13edc6
GRANDE-I7\idela <i.delamo@gmail.com>
2020-08-19 17:52:57 +0200

    Patrulla de waypoints a medias

--- RPG/Assets/scripts/Controller/AIController.cs
+++ RPG/Assets/scripts/Controller/AIController.cs
@@ -1,6 +1,7 @@
 ﻿using RPG.Combat;
 using RPG.Core;
 using RPG.Movement;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,12 +13,17 @@ namespace RPG.Control
     {
         [SerializeField] float chaseDistance = 5f;
         [SerializeField] float suspecionTime = 2f;
+        [SerializeField] PatrollPathScript patrollPath;
+        [SerializeField] float waypointTolerance = 1f;
         GameObject player;
         Fighter fighter;
         Health health;
         Vector3 guardLocation;
         Mover mover;
         float timeSinceLastSawPlayer = Mathf.Infinity; //tiempo desde que lo vi la ultima vez
+      
+
+
 
         private void Start()
         {
@@ -26,15 +32,13 @@ namespace RPG.Control
             health = GetComponent<Health>();
             guardLocation = transform.position;
             mover = GetComponent<Mover>();
+            
         }
 
 
         private void Update()
         {
-            if(health.IsDead) 
-            {                 
-                return; 
-            }
+            if(health.IsDead)  {return;}
 
            if (InAttackRangeOfPlayer() && fighter.CanAttack(player))
             {
@@ -45,20 +49,53 @@ namespace RPG.Control
             {
                 SuspicionBehavior();
             }
-
             else
+
             {
-                GuardBehavior();
+                PatrollBehavior();
             }
             timeSinceLastSawPlayer += Time.deltaTime;
         }
 
-        private void GuardBehavior()
+        private void PatrollBehavior()
         {
             // go back to guardLocation
+            Vector3 nextPosition = guardLocation;
+            if (patrollPath != null)
+            {
+                if (AtWaypoint())
+                {
+                    CycleWaypoint();
+                }
+                nextPosition = GetCurrentWaypoint();
+            }
+
+
             mover.StartMoveAction(guardLocation);
+         
             //fighter.Cancel();
         }
+
+
+        private bool AtWaypoint()
+        {
+            float distanceToWaypoint = Vector3.Distance(transform.position, GetCurrentWaypoint());
+            return  distanceToWaypoint< waypointTolerance;
+        }
+
+        private void CycleWaypoint()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Vector3 GetCurrentWaypoint()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+        
 
         private void SuspicionBehavior()
         {
