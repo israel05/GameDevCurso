f8323b86f3a10af67bb2510753795df9fb1020c8
GRANDE-I7\idela <i.delamo@gmail.com>
2020-08-28 19:56:24 +0200

    Añadida segunda escena

---
+++ RPG/Assets/scripts/cinematics/CinematicControllRemover.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using RPG.Core;
+using RPG.Control;
+
+namespace RPG.Cinematics
+{
+    public class CinematicControllRemover : MonoBehaviour
+    {
+
+        GameObject player;
+        private void Start()
+        {
+            GetComponent<PlayableDirector>().played += DisableControl;
+            GetComponent<PlayableDirector>().stopped += EnableControl;
+            player = GameObject.FindWithTag("Player");
+
+        }
+
+        void DisableControl(PlayableDirector pd)
+        {
+            
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
+
+            // porque si no, podria continuar con un ataque o movimiento previo a la cinemática
+
+        }
+
+        void EnableControl(PlayableDirector pd)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            print("EnableControl");
+            player.GetComponent<PlayerController>().enabled = true;
+        }
+    }
+}
