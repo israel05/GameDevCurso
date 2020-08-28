9aa1971b04d2b2e33e918f4cc75f2c10f7806937
GRANDE-I7\idela <i.delamo@gmail.com>
2020-08-28 15:58:10 +0200

    La cinematica se dispara solo una vez

---
+++ RPG/Assets/scripts/cinematics/CinematicTrigger.cs
@@ -0,0 +1,34 @@
+﻿using RPG.Control;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        bool disparadoDeAntes = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            print("Colision en cinemática");
+
+            
+            if (other.gameObject.tag == "Player")
+            {
+                print("Colision del jugador");
+                if (!disparadoDeAntes)
+                {
+                    GetComponent<PlayableDirector>().Play();
+                    print("Colision no reprodroducida aún");
+                    disparadoDeAntes = true;
+                }           
+            }
+            
+        }
+
+
+    }
+
+}
